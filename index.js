c83859055d310c1776aed9fb0a2301514408ec44
Kasey0998 <76687183+Kasey0998@users.noreply.github.com>
2025-05-27 10:38:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+import express from 'express';
+import bodyParser from ‘body-parser
+
+const app = express();
+const PORT = 8080
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
