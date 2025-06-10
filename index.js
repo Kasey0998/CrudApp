2dc694cb60e7ace6db4f15aa4eadb7e872648c8c
Kasey0998 <76687183+Kasey0998@users.noreply.github.com>
2025-06-10 21:06:18 +0100

    user module added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js';
+
 
 const app = express();
 const PORT = 8080
@@ -8,8 +10,12 @@ const PORT = 8080
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    console.log('[GET ROUTE]');
-    res.send('HELLO FROM /');
+    console.log("Hit registered on /");
+    res.send("Hello /");
+})
+
+app.get('/user',(req,res) => {
+    res.send(users);
 })
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
