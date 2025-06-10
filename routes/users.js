1e1e1f53ac90c8944ee722848353bf5c1c10b543
Krishna Sharma <kasey.gnr@icloud.com>
2025-06-10 22:17:16 +0100

    added post method to users

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,18 @@
 import express from 'express';
+import {v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
 
 
-// Mock database
-const users = [
-  {
-    first_name: 'John',
-    last_name: 'Doe',
-    email: 'johndoe@example.com',
-  },
-  {
-    first_name: 'Alice',
-    last_name: 'Smith',
-    email: 'alicesmith@example.com',
-  },
-];
+const users= []
 
-// Getting the list of users from the mock database
-router.get('/', (req, res) => {
-    res.send(users);
-})
+
+router.post('/users', (req, res) => {
+  const user=req.body;
+
+  users.push({ ...user, id: uuidv4() });
+  res.send(`User with the name ${user.first_name} added to the database!`);
+} );
+
 
 export default router
