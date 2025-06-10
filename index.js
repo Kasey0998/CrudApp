987a9033456939103adb15915b505cebbbbe9faa
Kasey0998 <76687183+Kasey0998@users.noreply.github.com>
2025-06-10 21:25:21 +0100

    user file linked to main

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ import userRoutes from './routes/users.js';
 const app = express();
 const PORT = 8080
 
+app.use('/users', userRoutes);
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     console.log("Hit registered on /");
     res.send("Hello /");
-})
-
-app.get('/user',(req,res) => {
-    res.send(users);
 })
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
